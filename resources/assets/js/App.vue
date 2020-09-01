7add60c08a1ff33e41a3240504ef6cb721b372d1
nadamson <n.adamson@jetmail.cc>
2020-09-01 15:51:59 +0300

    add filter_row,select_1_10,filter_select_items

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -4,75 +4,77 @@
             <h1>Houses</h1>
         </div>
 
+
+        <div id="filters">
+            <div class="filter_row" v-for="item in filter_select_items">
+                {{item}}
+                <select @change="read" v-model="filter[item]">
+                    <option
+                            v-for="item in select_1_10"
+                            :value="item"
+                            :key="item"
+                    >{{item}}
+                    </option>
+                </select>
+            </div>
+        </div>
+
         <house-component
                 v-for="house in houses"
                 v-bind="house"
                 :key="house.id"
-                @update="update"
-                @delete="del"
         ></house-component>
     </div>
 </template>
 
 <script>
-  function House({id, name, price, bedrooms,bathrooms,storeys,garages}) {
-    this.id = id;
-    this.name = name;
-    this.price = price;
-    this.bedrooms = bedrooms;
-    this.bathrooms = bathrooms;
-    this.storeys = storeys;
-    this.garages = garages;
-  }
-
-  import houseComponent from './components/Houses';
-
-  export default {
-    data() {
-      return {
-        houses: [],
-        mute: false
-      }
-    },
-    methods: {
-      async create() {
-        this.mute = true;
-        const { data } = await window.axios.get('/api/houses/create');
-        this.houses.push(new House(data));
-        this.mute = false;
-      },
-      async read() {
-        this.mute = true;
-        const { data } = await window.axios.get('/api/houses');
-        data.forEach(house => this.houses.push(new House(house)));
-        this.mute = false;
-      },
-      async update(id, color) {
-        this.mute = true;
-        await window.axios.put(`/api/houses/${id}`, { color });
-        this.houses.find(house => house.id === id).color = color;
-        this.mute = false;
-      },
-      async del(id) {
-        this.mute = true;
-        await window.axios.delete(`/api/houses/${id}`);
-        let index = this.houses.findIndex(house => house.id === id);
-        this.houses.splice(index, 1);
-        this.mute = false;
-      }
-    },
-    watch: {
-      mute(val) {
-        document.getElementById('mute').className = val ? "on" : "";
-      }
-    },
-    components: {
-      houseComponent
-    },
-    created() {
-      this.read();
+    function House({id, name, price, bedrooms, bathrooms, storeys, garages}) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.bedrooms = bedrooms;
+        this.bathrooms = bathrooms;
+        this.storeys = storeys;
+        this.garages = garages;
     }
-  }
+
+    import houseComponent from './components/Houses';
+
+    export default {
+        data() {
+            var select_1_10 = [''];
+            for(var i=1;i<=10;i++){
+                select_1_10.push(i);
+            }
+            return {
+                filter_select_items: ['bedrooms', 'bathrooms'],
+                select_1_10: select_1_10,
+                filter: {},
+                houses: [],
+                mute: false
+            }
+        },
+        methods: {
+            async read() {
+                this.mute = true;
+                this.houses = [];
+                const {data} = await window.axios.get('/api/houses?'+new URLSearchParams(this.filter));
+                data.forEach(house => this.houses.push(new House(house)));
+                this.mute = false;
+            },
+        },
+        watch: {
+            mute(val) {
+                document.getElementById('mute').className = val ? "on" : "";
+            }
+        },
+        components: {
+            houseComponent
+        },
+        created() {
+            this.read();
+        }
+    }
 </script>
 <style>
     #app {
@@ -81,5 +83,9 @@
 
     .heading h1 {
         margin-bottom: 0;
+    }
+    .filter_row{
+        display: inline-table;
+        margin-right: 10px;
     }
 </style>
